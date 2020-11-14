229a4ef9fb2198ec4934eb2a7fbf64f0dcfd1ca3
Wevison Ramalho Silva <wevison.ramalho@afferolab.com.br>
2020-11-14 12:46:05 -0300

    [ INITIAL ] Begging the project.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text">
+        Hello World!
+      </p>
+    </div>
+  );
+}
+
+export default App;
